144af6041998cb72df554c2af14cfd3dca24af35
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2026-03-22 00:12:08 +0200

    Delete src directory

--- src/lang/el/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const el: {
-    HELLO: string;
-    CLICK: string;
-    MORE: string;
-};
--- src/lang/en/index.ts
+++
@@ -1,6 +0,0 @@
-export const en = {
-  "HELLO": "Hello {#1}",
-  "CLICK": "Click on the {#1} and {#2} logos to learn more",
-  "MORE": "More info",
-};
-
