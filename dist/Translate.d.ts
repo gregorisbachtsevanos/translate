afb4932ff605cfae581af3637a8777d319d701f5
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2026-03-22 00:11:42 +0200

    Delete dist directory

--- dist/Translate.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Translate {
-    lang: string;
-    constructor(lang: string);
-    private getCurrentlyLang;
-    log: (_string: string, data?: any) => void;
-}
-export declare const translate: typeof Translate;
-export {};
--- dist/lang/en/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const en: {
-    HELLO: string;
-    CLICK: string;
-    MORE: string;
-};
